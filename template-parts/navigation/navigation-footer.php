8893ac52d5e34d22aa14f551332582be02e7ff7a
Dan Smith <dev@dotherightthing.co.nz>
2017-08-14 03:15:54 +1200

    Remove generic wrappers around navigation;

--- template-parts/navigation/navigation-footer.php
+++ template-parts/navigation/navigation-footer.php
@@ -12,45 +12,43 @@
  */
 
 ?>
-<div>
-    <nav role="navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'wpdtrt' ); ?>">
-        <?php
+<nav role="navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'wpdtrt' ); ?>">
+    <?php
+        /**
+         * wp_page_menu: 2.7.0+
+         * wp_nav_menu: 3.0.0+
+         */
+        wp_nav_menu( array(
             /**
-             * wp_page_menu: 2.7.0+
-             * wp_nav_menu: 3.0.0+
+             * Apply Appearance > Menus > Menu Structure
+             * otherwise fallback_cb is used
              */
-            wp_nav_menu( array(
-                /**
-                 * Apply Appearance > Menus > Menu Structure
-                 * otherwise fallback_cb is used
-                 */
-                'menu' => 'header-menu', // intentional duplicate to use same source
-                'container' => 'div',
-                'container_class' => '',
-                'container_id' => 'footer-nav',
-                'menu_class' => 'navigation',
-                'menu_id' => '',
-                'echo' => true,
-                /**
-                 * wp_page_menu() is sorted alphabetically
-                 */
-                'fallback_cb' => 'wp_page_menu',
-                'before' => '',
-                'after' => '',
-                'link_before' => '',
-                'link_after' => '',
-                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                'item_spacing' => 'preserve',
-                'depth' => 0,
-                'walker' => '',
-                /**
-                 * Theme location must be registered with register_nav_menu()
-                 * in order to be selectable by the user.
-                 * @todo why does this work when the full text is
-                 * "Footer Menu (mobile-first noscript fallback)"
-                 */
-                'theme_location' => 'Footer Menu',
-             ) );
-        ?>
-    </nav>
-</div>
+            'menu' => 'header-menu', // intentional duplicate to use same source
+            'container' => 'div',
+            'container_class' => '',
+            'container_id' => 'footer-nav',
+            'menu_class' => 'navigation',
+            'menu_id' => '',
+            'echo' => true,
+            /**
+             * wp_page_menu() is sorted alphabetically
+             */
+            'fallback_cb' => 'wp_page_menu',
+            'before' => '',
+            'after' => '',
+            'link_before' => '',
+            'link_after' => '',
+            'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+            'item_spacing' => 'preserve',
+            'depth' => 0,
+            'walker' => '',
+            /**
+             * Theme location must be registered with register_nav_menu()
+             * in order to be selectable by the user.
+             * @todo why does this work when the full text is
+             * "Footer Menu (mobile-first noscript fallback)"
+             */
+            'theme_location' => 'Footer Menu',
+         ) );
+    ?>
+</nav>
