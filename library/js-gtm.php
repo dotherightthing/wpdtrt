7c413630c32fbd07214563aa18ea544a4bbd8c06
Dan Smith <dev@dotherightthing.co.nz>
2017-12-23 12:01:58 +1300

    Disable buggy DIY GTM;

--- library/js-gtm.php
+++ library/js-gtm.php
@@ -8,7 +8,7 @@
  * @version 0.1.0
  */
 
-add_action( 'wp_enqueue_scripts', 'wpdtrt_js_gtm' );
+//add_action( 'wp_enqueue_scripts', 'wpdtrt_js_gtm' );
 
 function wpdtrt_js_gtm() {
 
@@ -23,18 +23,19 @@ function wpdtrt_js_gtm() {
   /**
    * @see http://kb.dotherightthing.dan/seo/google-tag-manager-gtm/
    */
-  wp_localize_script(
-    $header,
-    'wpdtrt_gtm_container_id',
-    get_field('wpdtrt_acf_gtm_container_id', 'option')
-  );
+  if ( function_exists('get_field') ) {
+    wp_localize_script(
+      $header,
+      'wpdtrt_gtm_container_id',
+      get_field('wpdtrt_acf_gtm_container_id', 'option')
+    );
 
-  // Google Tag Manager
-  wp_add_inline_script( $header, "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer',wpdtrt_gtm_container_id);" );
-
+    // Google Tag Manager
+    wp_add_inline_script( $header, "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer',wpdtrt_gtm_container_id);" );
+  }
 }
 ?>
