b4a0cf46e7d738e90cd5c6f88340fb91641a2cd2
Dan Smith <dev@dotherightthing.co.nz>
2019-10-14 14:25:39 +1300

    'use strict' is unnecessary

--- js/wpdtrt_footer.js
+++ js/wpdtrt_footer.js
@@ -17,14 +17,10 @@ const wpdtrt_ui = {
    * @see http://stackoverflow.com/a/28771425
    */
   touch_focus: () => {
-    "use strict";
-
     document.addEventListener("touchstart", () => {}, false);
   }
 };
 
 jQuery(document).ready( () => {
-  "use strict";
-
   wpdtrt_ui.touch_focus();
 });
